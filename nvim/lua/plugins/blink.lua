dbb29a6e830a98137f9547ae0362d2919649cb61
Shadoxx <shadoxx152@gmail.com>
2025-02-26 00:31:41 +0800

    nvim

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,24 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "enter",
+      ["<CR>"
+      ] = { "fallback" },
+      ["<Tab>"
+      ] = {
+        function(cmp)
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        "snippet_forward",
+        "fallback",
+      },
+      ["<S-Tab>"
+      ] = { "snippet_backward", "fallback" },
+    },
+  },
+}
